cac5a8921327c2cae446e84af042540459349678
Debmallya Bhattacharya <batbrain9392@batbrain9392-mac.local>
2020-05-18 13:16:42 +0100

    todo crud

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  console.log('App')
+  const [todos, setTodos] = useState<string[]>([
+    'Set up dev environment',
+    'Code the app',
+    'Deploy to Github Pages',
+  ])
+  const [todoInput, setTodoInput] = useState('')
+  const [editTodoIndex, setEditTodoIndex] = useState(-1)
 
-  return <h1>App</h1>
+  const onChangeInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoInput(event.target.value)
+  }
+
+  const onClickAddEditHandler = () => {
+    if (todoInput) {
+      setTodos((todos) =>
+        editTodoIndex === -1
+          ? [
+              ...todos.slice(0, editTodoIndex),
+              todoInput,
+              ...todos.slice(editTodoIndex + 1),
+            ]
+          : [...todos, todoInput]
+      )
+      setTodoInput('')
+    }
+    setEditTodoIndex(-1)
+  }
+
+  const onClickEditHandler = (todo: string, index: number) => {
+    setTodoInput(todo)
+    setEditTodoIndex(index)
+  }
+
+  const onClickDeleteHandler = (index: number) => {
+    setTodos((todos) => [...todos.slice(0, index), ...todos.slice(index + 1)])
+  }
+
+  return (
+    <>
+      <h1>App</h1>
+      <div>
+        <input type='text' value={todoInput} onChange={onChangeInputHandler} />
+        <button onClick={onClickAddEditHandler}>
+          {editTodoIndex === -1 ? 'edit' : 'add'} todo
+        </button>
+      </div>
+      {todos.length ? (
+        <>
+          <h3>Todo list</h3>
+          <ol>
+            {todos.map((todo, i) => (
+              <li key={i}>
+                <span style={{ marginRight: 15 }}>{todo}</span>
+                <button onClick={() => onClickEditHandler(todo, i)}>
+                  edit
+                </button>
+                <button onClick={() => onClickDeleteHandler(i)}>delete</button>
+              </li>
+            ))}
+          </ol>
+        </>
+      ) : (
+        <p>No todos for today</p>
+      )}
+    </>
+  )
 }
 
 export default App
