2c897bf93bed50c598e1085c568b8077e9cbfe57
Debmallya Bhattacharya <batbrain9392@batbrain9392-mac.local>
2020-05-18 12:11:23 +0100

    PostInfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import AllPosts from './AllPosts'
+// import AllPosts from './AllPosts'
+import PostInfo from './PostInfo'
 
 function App() {
   console.log('App')
 
-  return <AllPosts />
+  return (
+    <>
+      {/* <AllPosts /> */}
+      <PostInfo />
+    </>
+  )
 }
 
 export default App
