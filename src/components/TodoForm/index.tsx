3a77ac5a332528401b4cc61c25e30e967ba7dad9
Debmallya Bhattacharya <batbrain9392@batbrain9392-mac.local>
2020-05-20 08:50:13 +0100

    material form

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,4 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import InputBase from '@material-ui/core/InputBase'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import UpdateIcon from '@material-ui/icons/Update'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  })
+)
 
 interface Props {
   onSubmit: (value: string) => void
@@ -6,17 +35,16 @@ interface Props {
 }
 
 const TodoForm = (props: Props) => {
+  const classes = useStyles()
   const [input, setInput] = useState('')
-  const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     if (props.editValue) {
       setInput(props.editValue)
-      inputRef.current?.focus()
     }
   }, [props.editValue])
 
-  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitHandler = (event: React.FormEvent<HTMLDivElement>) => {
     event.preventDefault()
     if (input) {
       props.onSubmit(input)
@@ -25,17 +53,26 @@ const TodoForm = (props: Props) => {
   }
 
   return (
-    <form onSubmit={onSubmitHandler}>
-      <input
+    <Paper component='form' className={classes.root} onSubmit={onSubmitHandler}>
+      <InputBase
+        className={classes.input}
+        placeholder='Add todo'
+        inputProps={{ 'aria-label': 'add todo' }}
         type='text'
         required
+        autoFocus
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        autoFocus
-        ref={inputRef}
       />
-      <button type='submit'>{props.editValue ? 'update' : 'create'}</button>
-    </form>
+      <Divider className={classes.divider} orientation='vertical' />
+      <IconButton
+        color='primary'
+        className={classes.iconButton}
+        aria-label='add button'
+        type='submit'>
+        {props.editValue ? <UpdateIcon /> : <AddBoxIcon />}
+      </IconButton>
+    </Paper>
   )
 }
 
