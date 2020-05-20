fd24c60aadca33cbc72cd70c596291b74d099bd1
Debmallya Bhattacharya <batbrain9392@batbrain9392-mac.local>
2020-05-20 09:13:08 +0100

    material list

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import TodoItem from '../TodoItem'
 import { Todo } from 'types'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      margin: theme.spacing(4, 0, 2),
+    },
+  })
+)
 
 interface Props {
   todos: Todo[]
@@ -9,20 +20,22 @@ interface Props {
 }
 
 const TodoList = (props: Props) => {
+  const classes = useStyles()
+
   return props.todos.length ? (
-    <>
-      <h3>Todo list</h3>
-      <ol>
-        {props.todos.map((todo) => (
+    <div className={classes.root}>
+      <List>
+        {props.todos.map((todo, i) => (
           <TodoItem
             key={todo.id}
+            index={i + 1}
             value={todo.name}
             onEdit={() => props.onEdit(todo.id)}
             onDelete={() => props.onDelete(todo.id)}
           />
         ))}
-      </ol>
-    </>
+      </List>
+    </div>
   ) : (
     <p>No todos for today</p>
   )
